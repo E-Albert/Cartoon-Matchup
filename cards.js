696f8182449c31d1ec01138646107a3817bb0a04
E-Albert <magicspoon123@gmail.com>
2023-11-13 19:15:51 -0500

    imported powerpuff girl cards

---
+++ cards.js
@@ -0,0 +1,14 @@
+const cards = [
+  {
+    name: "powerpuff-buttercup",
+    img: "assets/cards/1.svg",
+  },
+  {
+    name: "powerpuff-blossom",
+    img: "assets/cards/1.svg",
+  },
+  {
+    name: "powerpuff-bubbles",
+    img: "assets/cards/1.svg",
+  },
+];
--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-//DOM elements
 
+
+//DOM elements
 let form = document.querySelector('#form');
 let cardContainer = document.querySelector('#card-container');
 let openingPage = document.querySelector('#opening-page')
 
+// global variables
+let show;
+let mode; 
+
+//event listeners
 form.addEventListener('submit', function (e) {
     e.preventDefault()
 
-    let show = document.querySelector('#show').value
-    let mode = document.querySelector('#mode').value
+    show = document.querySelector('#show').value
+    mode = document.querySelector('#mode').value
+
     console.log('Show: ' + show)
     console.log('Mode: ' + mode)
     console.log('Form button is responding')
 
-    
+    form.classList.toggle('active')
+
 })
+
+function populateGrid(show, mode) {
+    if (show === 'powerpuff') {
+        let card = document.createElement('div')
+        
+    }
+}
