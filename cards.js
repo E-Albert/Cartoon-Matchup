04be395620d6089c54ead27a4224a4427947f78a
E-Albert <magicspoon123@gmail.com>
2023-11-29 13:47:27 -0500

    renamed show options

--- cards.js
+++ cards.js
@@ -2,98 +2,122 @@ const cards = [
   {
     name: "powerpuff-buttercup",
     img: "assets/cards/1.svg",
+    show: "powerpuff",
   },
   {
     name: "powerpuff-blossom",
     img: "assets/cards/2.svg",
+    show: "powerpuff",
   },
   {
     name: "powerpuff-bubbles",
     img: "assets/cards/3.svg",
+    show: "powerpuff",
   },
   {
     name: "powerpuff-mojojojo",
     img: "assets/cards/4.svg",
+    show: "powerpuff",
   },
   {
     name: "powerpuff-him",
     img: "assets/cards/5.svg",
+    show: "powerpuff",
   },
   {
     name: "powerpuff-fuzzylumpkins",
     img: "assets/cards/6.svg",
+    show: "powerpuff",
   },
   {
     name: "powerpuff-princessmorbucks",
     img: "assets/cards/7.svg",
+    show: "powerpuff",
   },
   {
     name: "powerpuff-sedusa",
     img: "assets/cards/8.svg",
+    show: "powerpuff",
   },
   {
     name: "powerpuff-professorutonium",
     img: "assets/cards/9.svg",
+    show: "powerpuff",
   },
   {
     name: "powerpuff-themayor",
     img: "assets/cards/10.svg",
+    show: "powerpuff",
   },
   {
     name: "powerpuff-mskeane",
     img: "assets/cards/11.svg",
+    show: "powerpuff",
   },
   {
     name: "powerpuff-mssarahbellum",
     img: "assets/cards/12.svg",
+    show: "powerpuff",
   },
   {
     name: "powerpuff-brick",
     img: "assets/cards/13.svg",
+    show: "powerpuff",
   },
   {
     name: "powerpuff-boomer",
     img: "assets/cards/14.svg",
+    show: "powerpuff",
   },
   {
     name: "powerpuff-butch",
     img: "assets/cards/15.svg",
+    show: "powerpuff",
   },
   {
     name: "powerpuff-theboogieman",
     img: "assets/cards/16.svg",
+    show: "powerpuff",
   },
   {
     name: "powerpuff-ace",
     img: "assets/cards/17.svg",
+    show: "powerpuff",
   },
   {
     name: "powerpuff-snake",
     img: "assets/cards/18.svg",
+    show: "powerpuff",
   },
   {
     name: "powerpuff-bigbilly",
     img: "assets/cards/19.svg",
+    show: "powerpuff",
   },
   {
     name: "powerpuff-grubber",
     img: "assets/cards/20.svg",
+    show: "powerpuff",
   },
   {
     name: "powerpuff-lilarturo",
     img: "assets/cards/21.svg",
+    show: "powerpuff",
   },
   {
     name: "powerpuff-bossman",
     img: "assets/cards/22.svg",
+    show: "powerpuff",
   },
   {
     name: "powerpuff-slim",
     img: "assets/cards/23.svg",
+    show: "powerpuff",
   },
   {
     name: "powerpuff-junior",
     img: "assets/cards/24.svg",
+    show: "powerpuff",
   },
 ];
 
--- script.js
+++ script.js
@@ -1,40 +1,68 @@
-
-
 //DOM elements
-let form = document.querySelector('#form');
-let cardContainer = document.querySelector('#card-container');
-let openingPage = document.querySelector('#opening-page')
+let form = document.querySelector("#form");
+let cardContainer = document.querySelector("#card-container");
+let openingPage = document.querySelector("#opening-page");
 
 // global variables
 let show;
-let mode; 
+let mode;
 
 //event listeners
-form.addEventListener('submit', function (e) {
-    e.preventDefault()
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    show = document.querySelector('#show').value
-    mode = document.querySelector('#mode').value
+  show = document.querySelector("#show").value;
+  mode = document.querySelector("#mode").value;
 
-    console.log('Show: ' + show)
-    console.log('Mode: ' + mode)
-    console.log('Form button is responding')
+  console.log("Show: " + show);
+  console.log("Mode: " + mode);
+  console.log("Form button is responding");
 
-    form.classList.toggle('active')
-    populateGrid(show, mode)
-})
+  form.classList.toggle("active");
+  populateGrid(show, mode);
+});
+
+//functions
+function randomShuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
 
 function populateGrid(show, mode) {
-    if (show === 'powerpuff') {
+    //filter cards
+    //slice cards
+    //double cards
+    //randomize cards
+    //return cards
+    let filteredArray = cards.filter((card) => card.show === show);
 
-        for (i = 0; i < cards.length; i++) {
+    console.log(filteredArray)
 
-            const card = document.createElement("img");
-            card.setAttribute("src", `assets/cards/${i+1}.svg`);
-            card.setAttribute('class', 'card')
-            console.log(card)
-            cardContainer.appendChild(card);
-        }
-        
+    let shuffledCards = randomShuffle(filteredArray)
+    console.log(shuffledCards)
+
+    if (mode === 'easy') {
+        return shuffledCards.slice(0, 9)
     }
+
+   
+//   if (show === "powerpuff") {
+//        filteredArray = cards.filter((card) => card.show === "powerpuff");
+//       console.log(filteredArray)
+//     }
+
+//       let shuffledArray = randomShuffle(filteredArray)
+//       console.log(shuffledArray)
+      
+    // for (i = 0; i < cards.length; i++) {
+
+    //     const card = document.createElement("img");
+    //     card.setAttribute("src", `assets/cards/${i+1}.svg`);
+    //     card.setAttribute('class', 'card')
+    //     console.log(card)
+    //     cardContainer.appendChild(card);
+    // }
 }
